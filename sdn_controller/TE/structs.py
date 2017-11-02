a6634fcdf678f62f9e491a38cfd34c80d69cbdfb
Binh Nguyen <binh@cs.utah.edu>
2017-11-01 18:17:00 -0600

    Monitor active neighbors, snapshot

--- sdn_controller/TE/structs.py
+++ sdn_controller/TE/structs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2.7
 import logging as LOG
+import time, sys
 
 MSG_TYPES = { 1L: "HELLO",
               2L: "DBDESC",
@@ -13,6 +14,7 @@ class V(object):
 		self.ID = kwargs['ID']
 		self.IntraAdjs = kwargs['IntraAdjs']
 		self.Prefixes = kwargs['Prefixes']
+		self.ActiveNbs = kwargs['ActiveNbs']
 
 class IntraAdj(object):
 	def __init__(self, **kwargs):
@@ -25,6 +27,7 @@ class IntraAdj(object):
 		self.SrcInterfaceID = kwargs['SrcInterfaceID']
 		self.SrcInterfaceAddr = kwargs['SrcInterfaceAddr']
 		self.DstInterfaceID = kwargs['DstInterfaceID']
+		self.D = 0	#mark 1 if the adj is deleted
 		#self.DstInterfaceAddr = kwargs['DstInterfaceAddr']
 
 	def _update(self, src_router_id = None, dst_router_id = None, src_intf_id = None, dst_intf_id = None, src_router_prefixes = None, src_router_lladdr = None, W = None):
@@ -55,6 +58,7 @@ class IntraAdj(object):
 			
 	def print_me(self):
 		LOG.info("LS:\n"
+			"\t	Mark deleted:%s\n" \
 			"\t	LSID:%s\n" 
 			"\t	SrcRouterID:%s\n" 
 			"\t	DstRouterID:%s\n" 
@@ -62,10 +66,11 @@ class IntraAdj(object):
 			"\t	Src/Dst Interface IDs:%s, %s\n" 
 			"\t	Src Interface Address:%s\n" 
 			"\t	W:%s\n"
-			"\t	Prefixes:%s\n" % (self.LSID, self.SrcRouterID, self.DstRouterID, self.DstRouter, self.SrcInterfaceID, self.DstInterfaceID, self.SrcInterfaceAddr, self.W, self.Prefixes) 
+			"\t	Prefixes:%s\n" % (self.D, self.LSID, self.SrcRouterID, self.DstRouterID, self.DstRouter, self.SrcInterfaceID, self.DstInterfaceID, self.SrcInterfaceAddr, self.W, self.Prefixes) 
 		)
 	def str_me(self):
 		return "LS:\n"\
+			"\t	Mark deleted:%s\n" \
 			"\t	LSID:%s\n" \
 			"\t	SrcRouterID:%s\n" \
 			"\t	DstRouterID:%s\n" \
@@ -73,21 +78,35 @@ class IntraAdj(object):
 			"\t	Src/Dst Interface IDs:%s, %s\n" \
 			"\t	Src Interface Address:%s\n" \
 			"\t	W:%s\n"\
-			"\t	Prefixes:%s\n" % (self.LSID, self.SrcRouterID, self.DstRouterID, self.DstRouter, self.SrcInterfaceID, self.DstInterfaceID, self.SrcInterfaceAddr, self.W, self.Prefixes) 
+			"\t	Prefixes:%s\n" % (self.D, self.LSID, self.SrcRouterID, self.DstRouterID, self.DstRouter, self.SrcInterfaceID, self.DstInterfaceID, self.SrcInterfaceAddr, self.W, self.Prefixes) 
 
 
 class G(object):
+	NB_REFRESH_INTERVAL = 60 #in seconds 
 	def __init__(self, **kwargs):
 		self.G = {}
+		self.neighbor_last_refresh = sys.maxint
 	def getG(self):
 		return self.G
 	def addV(self, V):
 		if V:
 			self.G[V.ID] = V
 	def getV(self, ID):
-		return self.G[ID]
+		if ID in self.G:
+			return self.G[ID]
+		return None
 	def delV(self, ID):
 		del self.G[ID]
+	def markDeleteAdj(self, src_router_id, dst_router_id):
+		index = 0
+		for adj in self.G[src_router_id].IntraAdjs:
+			if adj.LSID != None and adj.LSID != 0 and adj.DstRouterID == dst_router_id:	#a bit conservative here.
+				#del self.G[src_router_id].IntraAdjs[index]
+				self.G[src_router_id].IntraAdjs[index].D = 1
+			index += 1
+		if self.G[src_router_id].IntraAdjs == None:
+			self.G[src_router_id].IntraAdjs = []
+
 	def deleteAdj(self, src_router_id, dst_router_id):
 		index = 0
 		for adj in self.G[src_router_id].IntraAdjs:
@@ -97,9 +116,34 @@ class G(object):
 		if self.G[src_router_id].IntraAdjs == None:
 			self.G[src_router_id].IntraAdjs = []
 
+	def addAdj(self, src_router_id, dst_router_id):
+		if src_router_id not in G:
+			return 1
+		src_router = self.G[src_router_id]
+		dst_router = V(ID=dst_router_id, IntraAdjs = [], Prefixes = [], ActiveNbs = {})
+		#src->dst
+		src_router_adj = IntraAdj(LSID=None, W=None, Prefixes=None, SrcRouterID=src_router_id, DstRouterID = dst_router_id, DstRouter=dst_router, SrcInterfaceID=None, SrcInterfaceAddr=None, DstInterfaceID=None)
+		src_router.IntraAdjs.append(src_router_adj)
+		#dst->src
+		dst_router_adj = IntraAdj(LSID=None, W=None, Prefixes=None, SrcRouterID=dst_router_id, DstRouterID = src_router_id, DstRouter=src_router, SrcInterfaceID=None, SrcInterfaceAddr=None, DstInterfaceID=None)
+		dst_router.IntraAdjs.append(dst_router_adj)
+		return 0
+
 
 	def print_me(self):
 		for ID in self.G:
 			LOG.info("RouterID:%s\n" % (ID))
 			for adj in self.G[ID].IntraAdjs:
-				LOG.info("\tsrcRtrID:%s,LSID:%s,W:%s,Prefixes:%s,srcIntfID:%s,srcIntfAddr:%s,dstIntfID:%s,dstRtrId:%s\n" % (adj.SrcRouterID, adj.LSID, adj.W, adj.Prefixes, adj.SrcInterfaceID, adj.SrcInterfaceAddr, adj.DstInterfaceID, adj.DstRouterID))
+				if adj.D == 0:
+					LOG.info("\tsrcRtrID:%s,LSID:%s,W:%s,Prefixes:%s,srcIntfID:%s,srcIntfAddr:%s,dstIntfID:%s,dstRtrId:%s\n" % (adj.SrcRouterID, adj.LSID, adj.W, adj.Prefixes, adj.SrcInterfaceID, adj.SrcInterfaceAddr, adj.DstInterfaceID, adj.DstRouterID))
+
+	def printNB(self):
+		ret = ""
+		for ID in self.G:
+			ret += "RouterID:%s, Active NBs:%s\n" % (ID, self.G[ID].ActiveNbs)
+		return ret
+
+	def clear_active_nbs(self):
+		for ID in self.G:
+			self.G[ID].ActiveNBs = {}
+
--- sdn_controller/TE/te_controller.py
+++ sdn_controller/TE/te_controller.py
@@ -32,6 +32,7 @@ from northbound_actions import Actions as Actions
 from sr_flows_mgmt import SR_flows_mgmt as SR_flows_mgmt
 from structs import *
 from ospfv3 import *
+import time
 
 LOG = logging.getLogger('ryu.app.Te_controller')
 LOG.setLevel(logging.DEBUG)
@@ -51,6 +52,9 @@ class Te_controller(ControllerBase):
 		if MSG_TYPES[t] == "LSUPD":
 			v = data['V']['V']
 			lsas = v['LSAS']
+			graph = Te_controller.graph
+			graph.clear_active_nbs()	#clear active neighbors to get new active neighbors
+			graph.neighbor_last_refresh = time.time() #Start the refresh timer after receiving a LSUPD (likely link change detected)
 			#LOG.debug("Received LSUDP=%s" % v)
 			for k in lsas:
 				lsa = lsas[k]
@@ -68,22 +72,81 @@ class Te_controller(ControllerBase):
 					self._process_intraareaprefix_lsa(lsa['V'], lsid, advrtr)
 				else:
 					LOG.warn("Received Unknown LSA, type:%s, lsid:%s, advrtr:%s" % (t, lsid, advrtr))
+		elif MSG_TYPES[t] == "HELLO":
+			v = data['V']['V']
+			src_router_id = data['V']['RID']
+			desig_router_id = v['DESIG']
+			nbors = v['NBORS']
+			self._process_hello(src_router_id, desig_router_id, nbors)
+
 		return Response(status=500)
 	
+
+	def _process_hello(self, src_router_id, desig_router_id, nbors):
+		LOG.info("**HELLO**: src_router_id:%s, designated_router:%s, nbors:%s" % (src_router_id, desig_router_id, nbors))
+		graph = Te_controller.graph
+		g = graph.getG()
+		if len(nbors) == 0 or src_router_id not in g:
+			return
+		for nb in nbors:
+			if nb not in g[src_router_id].ActiveNbs:
+				g[src_router_id].ActiveNbs[nb] = 1
+			if src_router_id not in g[nb].ActiveNbs:
+				g[nb].ActiveNbs[src_router_id] = 1
+
+		refresh_timer = time.time()-graph.neighbor_last_refresh 
+		if refresh_timer > graph.NB_REFRESH_INTERVAL: #refresh the active neighbors graph periodically
+			self._update_active_nbs_graph(g)
+			graph.neighbor_last_refresh = time.time()
+		
+	#not used
+	def _is_active_nbs_changed(self, router_id, new_nbs):
+		g = Te_controller.graph.getG()
+		router_nbs = g.getV(src_router_id).ActiveNbs
+		if len(new_nbs) != len(router_nbs) or len(set(router_nbs) & set(new_nbs)) > 0:
+			return 1
+		return 0
+	#not used
+	def _update_active_nbs(self, router_id, new_nbs):
+		g = Te_controller.graph.getG()
+		router_nbs = g.getV(src_router_id).ActiveNbs
+		for nb in router_nbs:
+			if nb not in new_nbs:#delete adj
+				Te_controller.graph.deleteAdj(router_id, nb)
+		for nb in new_nbs:
+			if nb not in router_nbs: #add adj
+				Te_controller.graph.addAdj(router_id, nb)
+		LOG.debug("Updated active nbs graph")
+		
+	def _update_active_nbs_graph(self, g):
+		LOG.info("Updating active neighbors ...")
+		LOG.debug(Te_controller.graph.printNB())
+		for router_id in g:
+			active_nbs = g[router_id].ActiveNbs
+			router_nbs = {}
+			for adj in g[router_id].IntraAdjs:
+				router_nbs[adj.DstRouterID] = 1
+				if adj.DstRouterID not in active_nbs: #delete the neighbor that is no longer active
+					Te_controller.graph.deleteAdj(router_id, adj.DstRouterID)
+			for active_nb in active_nbs:
+				if active_nb not in router_nbs: #add the active neighbor that is now active
+					Te_controller.graph.addAdj(router_id, nb)
+		return 0
+
 	#Logic:
 	#	- If router does not exist, create a new one.
 	#	- If router existed, delete all existing adjs that aren't specified in the LSA.
 	#	- Create adjs that don't exist and are specified in the LSA.
 	def _process_rtr_lsa(self, lsa, lsid, advrtr):
-		LOG.info("Router LSA: lsid:%s, advrtr:%s" % (lsid, advrtr))
-		LOG.info("Router LSA: %s\n\n" % lsa)
+		LOG.info("**Router LSA**: lsid:%s, advrtr:%s" % (lsid, advrtr))
+		LOG.info("**Router LSA**: %s\n\n" % lsa)
 		src_router_adjs = defaultdict(list)
 		src_router_id = advrtr
 		src_router = self._fetch_router(src_router_id)
 		if src_router:
 			pass
 		else:
-			src_router = V(ID=src_router_id, IntraAdjs = [], Prefixes = [])
+			src_router = V(ID=src_router_id, IntraAdjs = [], Prefixes = [], ActiveNbs = {})
 
 		#construct adjs of the 2 endpoint routers
 		#Note: if there is an ADJ from router 1 -> router 2, then there is automatically another ADJ from router 2 -> router 1.
@@ -99,13 +162,13 @@ class Te_controller(ControllerBase):
 			src_intf_id = intf['INTERFACEID']
 			dst_intf_id = intf['NBINTERFACEID']
 			#self._remember_adjs(src_router_adjs, src_router_id, dst_router_id)
-			src_router_adjs[src_router_id].append(dst_router_id)
+			#src_router_adjs[src_router_id].append(dst_router_id)
 			src_router_adj = self._fetch_adj(src_router_id, dst_router_id, src_intf_id)
 			dst_router_adj = self._fetch_adj(dst_router_id, src_router_id, dst_intf_id)
 			if dst_router: #update
 				pass 
 			else:	#new
-				dst_router = V(ID=dst_router_id, IntraAdjs = [], Prefixes = [])
+				dst_router = V(ID=dst_router_id, IntraAdjs = [], Prefixes = [], ActiveNbs = {})
 			if src_router_adj: #update
 				if 0 != self._update_adj(src_router_id, dst_router_id, src_intf_id, dst_intf_id, None, None, w):
 					LOG.warn("Can't update adj (%s, %s)" % (src_router_id, dst_router_id))
@@ -134,8 +197,8 @@ class Te_controller(ControllerBase):
 		
 
 	def _process_link_lsa(self, lsa, lsid, advrtr):
-		LOG.info("Link LSA: lsid:%s, advrtr:%s" % (lsid, advrtr))
-		LOG.info("Link LSA: %s\n\n" % lsa)
+		LOG.info("**Link LSA**: lsid:%s, advrtr:%s" % (lsid, advrtr))
+		LOG.info("**Link LSA**: %s\n\n" % lsa)
 		src_router_id = advrtr
 		src_router = self._fetch_router(src_router_id)
 		prefixes = lsa['prefixes']
@@ -143,7 +206,7 @@ class Te_controller(ControllerBase):
 		if src_router:
 			src_router.Prefixes = prefixes
 		else:
-			src_router = V(ID=src_router_id, IntraAdjs = [], Prefixes = prefixes)
+			src_router = V(ID=src_router_id, IntraAdjs = [], Prefixes = prefixes, ActiveNbs = {})
 
 		src_router_adj = self._fetch_adj(src_router_id, None, lsid)
 		if src_router_adj:
@@ -164,8 +227,8 @@ class Te_controller(ControllerBase):
 
 	def _process_network_lsa(self, lsa, lsid, advrtr):
 		#TODO: delete a router if offline
-		LOG.info("Network LSA: lsid:%s, advrtr:%s" % (lsid, advrtr))
-		LOG.info("Network LSA: %s\n\n"% lsa)
+		LOG.info("**Network LSA**: lsid:%s, advrtr:%s" % (lsid, advrtr))
+		LOG.info("**Network LSA**: %s\n\n"% lsa)
 		src_router_id = advrtr
 		for rtr in lsa['RTRS']:
 			if rtr == src_router_id:
@@ -179,11 +242,11 @@ class Te_controller(ControllerBase):
 			if src_router:
 				pass
 			else:
-				src_router = V(ID=src_router_id, IntraAdjs = [], Prefixes = [])
+				src_router = V(ID=src_router_id, IntraAdjs = [], Prefixes = [], ActiveNbs = {})
 			if dst_router:
 				pass
 			else:
-				dst_router = V(ID=dst_router_id, IntraAdjs = [], Prefixes = [])
+				dst_router = V(ID=dst_router_id, IntraAdjs = [], Prefixes = [], ActiveNbs = {})
 
 			if src_router_adj: #update adj
 				if 0 != self._update_adj(src_router_id, dst_router_id, src_intf_id, None):
@@ -210,13 +273,13 @@ class Te_controller(ControllerBase):
 		reflsid = lsa['reflsid']
 		src_router_id = lsa['refadvrouter']
 		prefixes = lsa['prefixes']
-		LOG.info("Intra Area Prefix LSA: lsid:%s, advrtr:%s, reflsid:%s, refadvrouterid:%s" % (lsid, advrtr, reflsid, src_router_id))
-		LOG.info("Intra Area Prefix LSA: %s\n\n" % lsa)
+		LOG.info("**Intra Area Prefix LSA**: lsid:%s, advrtr:%s, reflsid:%s, refadvrouterid:%s" % (lsid, advrtr, reflsid, src_router_id))
+		LOG.info("**Intra Area Prefix LSA**: %s\n\n" % lsa)
 		src_router = self._fetch_router(src_router_id)
 		if src_router:
 			src_router.Prefixes = prefixes
 		else:
-			src_router = V(ID=src_router_id, IntraAdjs = [], Prefixes = prefixes)
+			src_router = V(ID=src_router_id, IntraAdjs = [], Prefixes = prefixes, ActiveNbs = {})
 
 		src_router_adj = self._fetch_adj(src_router_id, None, reflsid)
 		if src_router_adj:
@@ -245,14 +308,16 @@ class Te_controller(ControllerBase):
 		if router_id not in g:
 			return -1
 		for adj in g[router_id].IntraAdjs:
-			if not adj.DstRouterID in router_adjs[router_id]:
-				LOG.debug("Removing adj: router:%s, dst_router:%s, router_adjs:%s" % (router_id, adj.DstRouterID, router_adjs))
+			LOG.debug("Removing adj: router:%s, dst_router:%s, router_adjs:%s" % (router_id, adj.DstRouterID, router_adjs))
+			if adj.DstRouterID != None and not adj.DstRouterID in router_adjs[router_id]:
 				Te_controller.graph.deleteAdj(router_id, adj.DstRouterID)
 				if adj.DstRouterID != None:
 					Te_controller.graph.deleteAdj(adj.DstRouterID, router_id) #also delete the oposite adj
+					LOG.debug("Removed adj: router:%s, dst_router:%s, router_adjs:%s" % (adj.DstRouterID, router_id, router_adjs))
+				LOG.debug("Removed adj: router:%s, dst_router:%s, router_adjs:%s" % (router_id, adj.DstRouterID, router_adjs))
 		return 0
 
-	
+	#not used
 	def _is_adj_exist(self, src_router_id, dst_router_id):
 		g = Te_controller.graph.getG()
 		if src_router_id not in g:
@@ -275,6 +340,8 @@ class Te_controller(ControllerBase):
 			return None
 		for adj in g[src_router_id].IntraAdjs:
 			if dst_router_id == adj.DstRouterID:
+				if adj.D == 1:
+					adj.D = 0	#make this adj alive again!
 				return adj
 		return None
 
@@ -284,6 +351,8 @@ class Te_controller(ControllerBase):
 			return None
 		for adj in g[router_id].IntraAdjs:
 			if lsid == adj.LSID:
+				if adj.D == 1:
+					adj.D = 0	#make this adj alive again!
 				return adj
 		return None
 
