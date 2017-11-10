802835608fd1848fefda67c14f6c81d46acdb9b2
Binh Nguyen <binh@cs.utah.edu>
2017-11-09 23:13:24 -0700

    Reload page to update graph

--- sdn_controller/www/graph.js
+++ sdn_controller/www/graph.js
@@ -1,60 +1,62 @@
 //MODIFY this before run
 var url = "http://155.98.39.103:8080/ospf_monitor/get_topology";
+var graph = {};
+/*
+while (1){
+	graph = fetch_graph(url);
+	if (graph != null){
+		console.log(graph);
+	}
+	console.log("draw_graph");
+	draw_graph(graph);
+	sleep(2000);
+}
+*/
 
-var graph = (function() {
-	var json = null;
-	$.ajax({
-		'async': false,
-		'global': false,
-		'url': url,
-		'dataType': "json",
-		'success': function (data) {
-			json = data;
-		}
-	});
-	return json;
-})();
-
-
-
+graph = fetch_graph(url);
 if (graph != null){
 	console.log(graph);
 }
+console.log("draw_graph");
+draw_graph(graph);
+window.location.reload();
 
-var nodes = [];
-for (var key in graph){
-	if (graph.hasOwnProperty(key)) {	
-		nodes.push({
-				id: key,
-			    	label: key,
-			    	shape: 'circle',
-			    	color: '#6E6EFD'
-				});
+function draw_graph(graph){
+	var nodes = [];
+	for (var key in graph){
+		if (graph.hasOwnProperty(key)) {	
+			nodes.push({
+					id: key,
+					label: key,
+					shape: 'circle',
+					color: '#6E6EFD'
+					});
+		}
 	}
-}
-// create an array with nodes
-var nodes_vis = new vis.DataSet(nodes);
+	// create an array with nodes
+	var nodes_vis = new vis.DataSet(nodes);
 
-// create edges
-var visited_nodes = [];
-var edges = [];
-if (nodes.length > 0){
-	get_edges(graph[nodes[0].id], visited_nodes, edges, graph, nodes[0].id);
-}
-var edges_vis = new vis.DataSet(edges);
+	// create edges
+	var visited_nodes = [];
+	var edges = [];
+	if (nodes.length > 0){
+		get_edges(graph[nodes[0].id], visited_nodes, edges, graph, nodes[0].id);
+	}
+	var edges_vis = new vis.DataSet(edges);
 
-// create a network
-var container = document.getElementById('mynetwork');
-var data = {
-    nodes: nodes_vis,
-    edges: edges_vis
-};
-var options = {
-    nodes: {
-      shape: 'circle'
-    }
-};
-var network = new vis.Network(container, data, options);
+	// create a network
+	var container = document.getElementById('mynetwork');
+	var data = {
+	    nodes: nodes_vis,
+	    edges: edges_vis
+	};
+	var options = {
+	    nodes: {
+	      shape: 'circle'
+	    }
+	};
+	var network = new vis.Network(container, data, options);
+}
 
 //DFS, get all edges
 function get_edges(root, visited_nodes, edges, graph, ID){
@@ -96,4 +98,39 @@ function is_edge_existed(src, dst, edges){
 		}
 	}
 	return false;
+}
+
+function fetch_graph(url){
+	var graph = (function() {
+			var json = null;
+			$.ajax({
+				'async': false,
+				'global': false,
+				'url': url,
+				'dataType': "json",
+				'success': function (data) {
+					json = data;
+				}
+			});
+			return json;
+	})();
+	return graph;
+}
+
+async function check_update(url, graph){
+	var last_graph = graph;
+	while (JSON.stringify(graph) === JSON.stringify(last_graph)) {
+		console.log("fetch_graph");
+		last_graph = graph;
+		graph = fetch_graph(url);
+		//console.log("graph: ", graph);
+		sleep(2000);
+	}	
+	return graph;
+}
+
+function sleep(ms) {
+  var start = new Date().getTime(), expire = start + ms;
+  while (new Date().getTime() < expire) { }
+  return;
 }
