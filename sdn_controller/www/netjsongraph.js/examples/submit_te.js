86e511ec89719a2377edea40dcf142c1ca63cb0f
Binh Nguyen <binh@cs.utah.edu>
2017-11-15 12:59:22 -0700

    Webapp: node/adj segment works

--- sdn_controller/www/netjsongraph.js/examples/submit_te.js
+++ sdn_controller/www/netjsongraph.js/examples/submit_te.js
@@ -1,9 +1,8 @@
-
+//Submit REST http requests to the SDN controller to install the path entered by user in the HTTP form.
 function submit_te(){
 	var from = document.getElementById("from").value;
 	var to = document.getElementById("to").value;
 	var controller = document.getElementById("controller").value;
-	var url = controller+":8080/flow_mgmt/insert";
 	var seg_types = document.getElementsByName("seg_type");
 	var segs = document.getElementById("segs").value.split(",");
 	var reversed_segs = document.getElementById("segs").value.split(",").reverse();
@@ -11,41 +10,48 @@ function submit_te(){
 	//Facing corenetwork: port 1
 	//Facing host: port 2
 	//Segment routing encapsulation port: port 5
-	var in_port = 1;
-	var out_port = 2;
-	var sr_encap_port = 5;
-	var priority = 2;
+	const IN_PORT = 1;
+	const OUT_PORT = 2;
+	const SR_ENCAP_PORT = 5;
+	const PRIORITY = 2;
+	const ETH_TYPE= "0x86DD";
+	const FLOW_INSERT_API=":8080/flow_mgmt/insert"
+	const IS_DEBUG = 0;
 
+	//TODO: check if segments path is valid: eg, it starts with the "from" node and ends with the "to" node, etc.
+	check_valid_values(from, to, controller, segs);
+
+	var url = controller+FLOW_INSERT_API;
 	var src_dpid = get_dpid(from);
 	var dst_dpid = get_dpid(to);
-	window.alert("src dpid " + src_dpid + ", dst dpid " + dst_dpid + ", seg type " + seg_type + ", segs " + segs + ", reversed segs " + reversed_segs + ", controller " + controller);
 	var graph = fetch_graph(controller+":8080/ospf_monitor/get_topology_netjson");
-	//TODO: check if segments path is valid: eg, it starts with the "from" node and ends with the "to" node, etc.
 	var src_match = get_match(to, graph);
 	var dst_match = get_match(from, graph);
 	if (src_match.length == 0 || dst_match.length == 0){
 		window.alert("Can't find match for router " + to + " or " + from);
 		return;
 	}
-	window.alert("Src match: " + src_match + "\nDst match: " + dst_match);
+	if (IS_DEBUG==1)
+		window.alert("Src match: " + src_match + "\nDst match: " + dst_match);
 	var src_actions = get_actions(segs, seg_type, graph);
 	var dst_actions = get_actions(reversed_segs, seg_type, graph);
 	if (src_actions.length == 0 || dst_actions.length == 0){
 		window.alert("Can't find actions for path " + segs);
 		return;
 	}
-	//window.alert("Src actions: " + src_actions + "\nDst actions: " + dst_actions);
-	var method = "POST";
-	var src_post_data = construct_post_data(src_dpid, src_match, src_actions, in_port, out_port, priority);
-	var dst_post_data = construct_post_data(dst_dpid, dst_match, dst_actions, in_port, out_port, priority);
+	if (IS_DEBUG==1)
+		window.alert("Src actions: " + src_actions + "\nDst actions: " + dst_actions);
+	var src_post_data = construct_post_data(src_dpid, src_match, ETH_TYPE, src_actions, IN_PORT, SR_ENCAP_PORT, PRIORITY);
+	var dst_post_data = construct_post_data(dst_dpid, dst_match, ETH_TYPE, dst_actions, IN_PORT, SR_ENCAP_PORT, PRIORITY);
 	if (src_post_data.length == 0 || dst_post_data.length == 0){
 		window.alert("Can't construct post data for path " + segs);
 		return;
 	}
-	//window.alert("Src POST data: " + src_post_data.join("\n"));
-	//window.alert("Dst POST data: " + dst_post_data.join("\n"));
-	// You REALLY want shouldBeAsync = true.
-	// Otherwise, it'll block ALL execution waiting for server response.
+	if (IS_DEBUG==1){
+		window.alert("Src POST data: " + src_post_data.join("\n"));
+		window.alert("Dst POST data: " + dst_post_data.join("\n"));
+	}
+	//Otherwise, it'll block ALL execution waiting for server response.
 	var shouldBeAsync = true;
 
 	var request = new XMLHttpRequest();
@@ -55,24 +61,38 @@ function submit_te(){
 	   var data = request.responseText; // Returned data, e.g., an HTML document.
 	   console.log(data);
 	}
-	console.log(url);
-	request.open(method, url, shouldBeAsync);
 
-	//request.setRequestHeader("Content-Type", "application/json");
 
-	// Actually sends the request to the server.
+	//Install segment routing encapsulation rules on the OVSes.
+	var src_seg_post_data = construct_post_data(src_dpid, "", "", "", SR_ENCAP_PORT, OUT_PORT, PRIORITY);
+	var dst_seg_post_data = construct_post_data(dst_dpid, "", "", "", SR_ENCAP_PORT, OUT_PORT, PRIORITY);
+	send_POST_request(request, url, src_seg_post_data, shouldBeAsync);
+	if (IS_DEBUG==1)
+		window.alert("Sent src_seg_post_data " + src_seg_post_data);
+	send_POST_request(request, url, dst_seg_post_data, shouldBeAsync);
+	if (IS_DEBUG==1)
+		window.alert("Sent dst_seg_post_data " + dst_seg_post_data);
+
+	//Install matching rules before sending to the segment routing encapsulation port.
 	for (var i = 0; i < src_post_data.length; i++){
-		request.open(method, url, shouldBeAsync);
-		request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-		request.send(src_post_data[i]);
-		//window.alert("Sent src_post_data " + src_post_data[i]);
+		send_POST_request(request, url, src_post_data[i], shouldBeAsync);
+		if (IS_DEBUG==1)
+			window.alert("Sent src_post_data " + src_post_data[i]);
 	}
 	for (var i = 0; i < dst_post_data.length; i++){
-		request.open(method, url, shouldBeAsync);
-		request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-		request.send(dst_post_data[i]);
-		//window.alert("Sent dst_post_data " + dst_post_data[i]);
+		send_POST_request(request, url, dst_post_data[i], shouldBeAsync);
+		if (IS_DEBUG==1)
+			window.alert("Sent dst_post_data " + dst_post_data[i]);
 	}
+	window.alert("Done installing the path");
+}
+
+function send_POST_request(request, url, post_data, is_async){
+	request.open("POST", url, is_async);
+	//Send POST request with POST data in the format: "dpid=123&match=123&action=..."
+	request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	//request.setRequestHeader("Content-Type", "application/json");
+	request.send(post_data);
 }
 
 function get_segment_type_option(seg_types){
@@ -86,15 +106,10 @@ function get_segment_type_option(seg_types){
 	return seg_type;
 
 }
-function check_valid_parameters(controller, match){
-	if (is_valid_ipv6(match) == false){
-		window.alert("Match field " + match + "is an INVALID IPv6 address!");
-		return false;
-	}
-	if (is_valid_controller(controller) == false){
-		window.alert("Controller address " + controller + " is an INVALID address!");
-		return false;
-	}
+
+//TODO
+function check_valid_values(from, to, controller, segs){
+	return;
 }
 
 function construct_actions(segments){
@@ -111,40 +126,54 @@ function construct_actions(segments){
 	return ret.substring(0,ret.length-1);
 }
 
-function construct_post_data(dpid, match, actions, in_port, out_port, priority){
+function construct_ofp_rule(dpid, match, eth_type, actions, in_port, out_port, priority){
+	var fields = [];
+	fields.push("dpid="+dpid);
+	fields.push("priority="+priority);
+	fields.push("match="+construct_ofp_match_str(match, eth_type, in_port));
+	fields.push("actions="+construct_ofp_actions_str(actions, out_port));
+	return fields.join("&");
+}
+
+function construct_ofp_match_str(match, eth_type, in_port){
+	var match_fields = [];
+	if (match != "")
+		match_fields.push(match);
+	if (eth_type != "")
+		match_fields.push("eth_type="+eth_type);
+	if (in_port != "")
+		match_fields.push("in_port="+in_port);
+	return match_fields.join(",");
+}
+
+function construct_ofp_actions_str(actions, out_port){
+	var actions_fields = [];
+	if (actions != "")
+		actions_fields.push(actions);
+	if (out_port != "")
+		actions_fields.push("output="+out_port);
+	return actions_fields.join(",");
+}
+
+
+function construct_post_data(dpid, match, eth_type, actions, in_port, out_port, priority){
 	var ret = [];
-	if (dpid == "" || match == "" || actions == "" || out_port == "") {
+	if (dpid == "" || out_port == "" || priority == "") {
 		return ret;
 	}
 	var matches = match.split(",");
+	if (matches.length == 0){
+		ret.push(construct_ofp_rule(dpid, "", eth_type, actions, in_port, out_port, priority));
+	}
 	for (var i = 0; i < matches.length; i++){
-		var p = "";
-		p += "dpid="+dpid;
-		p += "&priority="+priority;
-		p += "&match="+matches[i]+",eth_type=0x86DD,in_port="+in_port;
-		p += "&actions="+actions+",output="+out_port;
-		ret.push(p);
+		ret.push(construct_ofp_rule(dpid, matches[i], eth_type, actions, in_port, out_port, priority));
 	}
 	return ret;
 }
 
-//TODO
-function is_valid_controller(controller){
-	if (controller.length > 0){
-		return true;
-	} 
-	return false
-}
 
 function is_valid_ipv6(ipv6){
  	return /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$|^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$|^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$/.test(ipv6);
-}
-
-//not used
-function reverse_str(segs){
-	var tokens = segs.split("");
-	var reversed_tokens = tokens.reverse();
-	return reversed_tokens.join("");
 }
 
 function get_dpid(node){
@@ -153,6 +182,9 @@ function get_dpid(node){
 
 //Generate the SDN 'match' field from the destination node.
 //By default the 'match' field matches all prefixes that the dst node has.
+//eg, if dst node has 2 prefixes: 200::/16, 2001::1234:/64 then the match
+//will be: ["ipv6_dst=200::/16", "ipv6_dst=2001::1234:/64"]
+//Return: list of ipv6 addresses
 function get_match(node, graph){
 	nodes = graph.nodes;
 	var ret = "";
@@ -181,6 +213,7 @@ function get_match(node, graph){
 }
 
 //Generate the SDN 'actions' field from a list of nodes describing the path.
+//Return: list of ipv6 addresses
 function get_actions(segs, seg_type, graph){
 	ret = "";
 	ipv6s = [];
